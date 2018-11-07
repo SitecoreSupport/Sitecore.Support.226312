17fd5326f31ee23998d802c3dfac1f212817265b
Paul Kravchenko <pk@sitecore.net>
2018-11-07 17:06:59 +0200

    Initial project

---
+++ src/Sitecore.Support.226312/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.226312")]
+[assembly: AssemblyProduct("Sitecore.Support.226312")]
+[assembly: ComVisible(false)]
